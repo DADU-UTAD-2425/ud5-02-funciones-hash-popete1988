43cb434273ffe046d988d80c3a398c494fc4f738
popete <popete88@gmail.com>
2025-05-12 11:33:41 +0200

    Commit final

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,23 @@ public class Main {
 			Scanner sc = new Scanner (System.in);
 			boolean sonIguales=false;
 			//1 - Obten la instancia con algoritmo MD5
-			MessageDigest md = null;
+			MessageDigest md = MessageDigest.getInstance("MD5");
 			
 			System.out.print("Introduzca la password: ");
 			byte[] pwdByte = sc.nextLine().getBytes();
 			//2 - Actualiza la instancia
-
+			md.update(pwdByte);
 			//3 - Calcula el resumen
-			byte[] resumenPwd = null;
+			byte[] resumenPwd = md.digest();
 			do {
 				System.out.print("Repetir password: ");
 				pwdByte = sc.nextLine().getBytes();
 				//4 - Limpia la instancia
-				
+				md.reset();
 				//5 - Actualiza la instancia
-				
+				md.update(pwdByte);
 				//6 - Calcula el resumen
-				byte[] resumenPwdRepetir = null;
+				byte[] resumenPwdRepetir = md.digest();
 				
 				sonIguales = Arrays.equals(resumenPwd,resumenPwdRepetir);
 				if (!sonIguales) {
@@ -32,8 +32,10 @@ public class Main {
 				}
 			} while (!sonIguales);
 			System.out.println("Fin del programa");
+			sc.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 }
